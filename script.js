733c811a22c50ef2c676a0fab287324edea0b2c5
Dikesh Kumar <117905520+Dikesh01@users.noreply.github.com>
2023-06-10 13:40:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+function playSound(keyCode) {
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${keyCode}"]`);
+  if (!audio) return;
+
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
+}
+
+function handleKeyDown(event) {
+  const keyCode = event.keyCode;
+  playSound(keyCode);
+}
+
+function removePlayingClass(event) {
+  if (event.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+window.addEventListener('keydown', handleKeyDown);
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removePlayingClass));
+
